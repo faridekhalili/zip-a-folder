4c3000eb06a0d153faadea9c624f4d9fafd7c947
Faridekhalili <khalili.f@northeastern.edu>
2026-01-02 21:50:14 -0500

    Add augmented test.

---
+++ test/augmented.test.ts
@@ -0,0 +1,42 @@
+'use strict';
+import 'jest-extended';
+import * as fs from 'fs';
+import * as rimraf from 'rimraf';
+import * as path from 'path';
+import {COMPRESSION_LEVEL, tar, zip, ZipAFolder as zipafolder} from '../lib/ZipAFolder';
+
+describe('Zip-A-Folder Test', function () {
+
+    beforeAll(() => {
+        rimraf.sync('test/*.tgz');
+        rimraf.sync('test/*.tar');
+        rimraf.sync('test/*.zip');
+    });
+
+    it('tar should invoke compress for COMPRESSION_LEVEL.uncompressed', async () => {
+      
+      /** 
+       * Sample 2:
+       * BlockStatement
+       * lib/ZipAFolder.ts:40:63
+       * -           if (o.compression === COMPRESSION_LEVEL.uncompressed) {
+       * -               await ZipAFolder.compress({src, targetFilePath: tarFilePath, format: 'tar', zipAFolderOptions});
+       * -           } else {
+       * +           if (o.compression === COMPRESSION_LEVEL.uncompressed) {} else {",
+      * */
+      
+      // Spy on the internal compress method
+      const compressSpy = jest.spyOn(zipafolder as any, 'compress');
+      const testTarPath = path.resolve(__dirname, 'testCompressCalled.tar');
+      
+      await tar(path.resolve(__dirname, 'data/'), testTarPath, { compression: COMPRESSION_LEVEL.uncompressed });
+      
+      // In the original code, compress should have been called once.
+      expect(compressSpy).toHaveBeenCalledTimes(1);
+      
+      // Clean up the spy
+      compressSpy.mockRestore();
+    });
+    
+});
+
